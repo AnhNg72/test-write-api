3b1e051f799a650c3f037aca474ac5f768df6dfb
Anh Ng <VanAnh.Nguyen@liebherr.com>
2026-05-19 15:47:59 +0200

    updated swagger info to v5

--- src/main/java/com/localintegrationapps/simpleapi/SimpleApiApplication.java
+++ src/main/java/com/localintegrationapps/simpleapi/SimpleApiApplication.java
@@ -14,9 +14,9 @@ public class SimpleApiApplication {
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 				.info(new Info()
-						.title("Updated Test Write API v5")
-						.version("5.0")
-						.description("Updated API documentation v5 for API that writes data to DB"));
+						.title("Updated Test Write API v6")
+						.version("6.0")
+						.description("Updated API documentation v6 for API that writes data to DB"));
 	}
 
 	public static void main(String[] args) {
