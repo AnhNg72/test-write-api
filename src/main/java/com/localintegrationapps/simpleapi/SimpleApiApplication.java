3532ed4e1ea0a669c9d6b2c371a18b1105e58bd2
Anh Ng <VanAnh.Nguyen@liebherr.com>
2026-05-19 15:05:25 +0200

    updated deployment and Swagger info 2

--- src/main/java/com/localintegrationapps/simpleapi/SimpleApiApplication.java
+++ src/main/java/com/localintegrationapps/simpleapi/SimpleApiApplication.java
@@ -14,9 +14,9 @@ public class SimpleApiApplication {
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 				.info(new Info()
-						.title("Updated Test Write API")
-						.version("2.0")
-						.description("Updated API documentation for API that writes data to DB"));
+						.title("Updated Test Write API v3")
+						.version("3.0")
+						.description("Updated API documentation v3 for API that writes data to DB"));
 	}
 
 	public static void main(String[] args) {
