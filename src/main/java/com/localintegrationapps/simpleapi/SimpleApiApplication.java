95101a5d54d7b05b3f73d3af76749c20315de225
Anh Ng <VanAnh.Nguyen@liebherr.com>
2026-05-20 11:00:47 +0200

    updated swagger doc version to v8

--- src/main/java/com/localintegrationapps/simpleapi/SimpleApiApplication.java
+++ src/main/java/com/localintegrationapps/simpleapi/SimpleApiApplication.java
@@ -14,9 +14,9 @@ public class SimpleApiApplication {
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 				.info(new Info()
-						.title("Updated Test Write API v7")
-						.version("7.0")
-						.description("Updated API documentation v7 for API that writes data to DB"));
+						.title("Updated Test Write API v8")
+						.version("8.0")
+						.description("Updated API documentation v8 for API that writes data to DB"));
 	}
 
 	public static void main(String[] args) {
